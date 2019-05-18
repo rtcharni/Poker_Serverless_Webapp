184b07df60d5ffd4c91b7e55c62188cc9bbe083c
elama09 <roman.tcharni@gmail.com>
2019-05-19 00:15:25 +0300

    Added user models

---
+++ serverless/functions/src/models/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+    username?: string;
+    password?: string;
+    money?: number;
+    statistics?: UserStatistics;
+}
+
+export interface UserStatistics {
+    biggest_win?: number;
+    draws?: number;
+    loses?: number;
+    money_record?: number;
+    total_games?: number;
+    wins?: number;
+}
