14c18cd0df8ad9e640455ca2130f407b6c3c093a
elama09 <roman.tcharni@gmail.com>
2019-05-19 21:49:01 +0300

    Implemented getUser function

---
+++ serverless/functions/src/onRequest-trigger/getUser.ts
@@ -0,0 +1,19 @@
+import * as functions from 'firebase-functions';
+import { getUserDataDB } from '../utils/firestore';
+import { User } from '../models/user';
+import { checkIsPasswordValid } from '../utils/bcrypt';
+
+export const getUser = functions.https.onRequest(async (request, response) => {
+    try {
+        const {username, password} = request.query;
+        const foundUser: User = await getUserDataDB(username);
+        if (foundUser) {
+            if (await checkIsPasswordValid(password, foundUser.password)) {
+                response.status(200).send({ user: foundUser, success: true });
+            }
+        }
+        response.send({ msg: `Invalid username or password..`, success: false });
+    } catch (error) {
+        response.status(500).send({});
+    }
+});
