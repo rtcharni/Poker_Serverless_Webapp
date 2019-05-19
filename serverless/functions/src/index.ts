d115c059b86e1f65d5458461584cc50df6a3f704
elama09 <roman.tcharni@gmail.com>
2019-05-19 21:49:32 +0300

    Small changes

--- serverless/functions/src/index.ts
+++ serverless/functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-export * from './onRequest-trigger/testFunc';
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-admin.initializeApp();
+export * from './onRequest-trigger/getUser';
+export * from './onRequest-trigger/createUser';
+export * from './onRequest-trigger/updateUser';
+
+// admin.initializeApp();
+admin.initializeApp(functions.config().firebase);
+
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
--- serverless/functions/src/onRequest-trigger/testFunc.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from 'firebase-functions';
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-
-export const testFunction = functions.https.onRequest((request, response) => {
-    const query: string = request.query.id;
-    response.send(`Hello from test function, query: ${query}`);
-});
