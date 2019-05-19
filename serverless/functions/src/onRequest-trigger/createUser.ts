3faa572b8ece400244f6eb5bf85c21bba58a2dd4
elama09 <roman.tcharni@gmail.com>
2019-05-19 21:49:14 +0300

    Implemented createUser function

--- serverless/functions/src/onRequest-trigger/createUser.ts
+++ serverless/functions/src/onRequest-trigger/createUser.ts
@@ -1,9 +1,16 @@
 import * as functions from 'firebase-functions';
+import { hashPassword } from '../utils/bcrypt';
+import { User } from '../models/user';
+import { addUserToDB } from '../utils/firestore';
 
-export const createUser = functions.https.onRequest((request, response) => {
-    const { username, password }: any = request.body;
-
-
-
-    response.status(200).send({msg: `New user - ${username} created!`});
+export const createUser = functions.https.onRequest(async (request, response) => {
+    try {
+        const { username, password } = request.body;
+        const hashed = await hashPassword(password);
+        const user: User = { money: 50, username, password: hashed, statistics: { biggest_win: 0, wins: 0, loses: 0, draws: 0, money_record: 0, total_games: 0 } }
+        await addUserToDB(user);
+        response.status(200).send({ msg: `User ${user.username} created!`, success: true });
+    } catch (error) {
+        response.status(500).send({msg: `Error happened while creating new user..`, success: false});
+    }
 });
