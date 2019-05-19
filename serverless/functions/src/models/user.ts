5b3ab5aab766a98474c69533647f7073b623ee3c
elama09 <roman.tcharni@gmail.com>
2019-05-19 22:10:40 +0300

    Small updates

--- serverless/functions/src/models/user.ts
+++ serverless/functions/src/models/user.ts
@@ -1,8 +1,8 @@
 export interface User {
-    username: string;
-    password: string;
-    money: number;
-    statistics: UserStatistics;
+    username?: string;
+    password?: string;
+    money?: number;
+    statistics?: UserStatistics;
 }
 
 export interface UserStatistics {
