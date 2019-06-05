aa9fd8c816456dde53edf4fd702717def51a4d02
elama09 <roman.tcharni@gmail.com>
2019-06-05 20:45:29 +0300

    Added router

--- pokerapp/src/router.ts
+++ pokerapp/src/router.ts
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Login from './views/Login.vue';
+import Signup from './views/Signup.vue';
+import About from './views/About.vue';
+import Game from './views/Game.vue';
+
 
 Vue.use(Router);
 
@@ -16,10 +21,22 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: About,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
+    },
+    {
+      path: '/game',
+      name: 'game',
+      component: Game,
     },
   ],
 });
