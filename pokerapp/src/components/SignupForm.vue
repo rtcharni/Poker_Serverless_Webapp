4806243f8acb651900782ca2e2786cab2be7bbeb
elama09 <roman.tcharni@gmail.com>
2019-06-09 20:24:51 +0300

    WIP finishing up signup page

--- pokerapp/src/components/SignupForm.vue
+++ pokerapp/src/components/SignupForm.vue
@@ -1,53 +1,60 @@
 <template>
   <div>
-    <v-card light>
-      <v-card-title primary-title>
-        <div>
-          <h3 class="display-2 font-weight-thin mb-3 mt-5">Signup</h3>
-        </div>
-      </v-card-title>
-      <v-card-text>
-        <v-form ref="loginform" v-model="valid" lazy-validation>
-          <v-text-field
-            v-model="username"
-            :counter="10"
-            :rules="usernameRules"
-            label="Choose username"
-            required
-          ></v-text-field>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-card light>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="display-2 font-font-font-weight-medium mt-2">Signup</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-form ref="loginform" v-model="valid" lazy-validation>
+              <v-text-field
+                v-model="username"
+                :counter="10"
+                :rules="usernameRules"
+                label="Choose username"
+                required
+              ></v-text-field>
 
-          <v-text-field v-model="password" :rules="passwordRules" label="Choose password" required></v-text-field>
+              <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                label="Choose password"
+                required
+              ></v-text-field>
 
-          <v-btn :disabled="!valid" color="success" @click="handleLoginClick">Create User</v-btn>
+              <v-btn :disabled="!valid" color="success" @click="handleSignupClick">Create User</v-btn>
 
-          <v-btn flat color="error" @click="handleResetClick">Clear</v-btn>
-        </v-form>
-      </v-card-text>
+              <v-btn flat color="error" @click="handleResetClick">Clear</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-layout>
+    </v-container>
 
-      <!-- <v-card-actions> -->
-      <!-- </v-card-actions> -->
-    </v-card>
-    <Snackbar 
-    v-bind:snackbar="snackbar"
-    v-bind:timeout="4000"
-    v-bind:text="'Username already exists...'"
-    v-bind:color="'error'"/>
+    <Snackbar
+      id="snackbar"
+      v-bind:snackbar="snackbar"
+      v-bind:timeout="0"
+      v-bind:text="'Username already exists...'"
+      v-bind:color="'error'"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Snackbar from './Snackbar.vue';
+import Snackbar from "./Snackbar.vue";
 
 export default Vue.extend({
   name: "signupform",
   components: {
-      Snackbar,
+    Snackbar
   },
   data: () => ({
-      snackbar: false,
-
-
+    snackbar: false,
     valid: false,
     username: "",
     usernameRules: [
@@ -61,14 +68,28 @@ export default Vue.extend({
     ]
   }),
   methods: {
-    handleLoginClick() {
+    async handleSignupClick() {
       if (this.$refs.loginform.validate()) {
-        console.log("VALIDATE");
-        //   this.snackbar = true
+        console.log(this);
+        const user = await (await fetch('GetUserURL')).json();
+        if (!user) {
+            // Create user, send api call, ok message -> redirect..
+            const headers: HeadersInit = {set: ('Content-Type','Application/json') }
+            const success = await fetch('CreateUser',{method: "POST", headers: {set: {'Content-Type':'Application/json'}}})
+        } else {
+            this.showAndHideSnackbar();
+            return;
+        }
       }
     },
     handleResetClick() {
       this.$refs.loginform.reset();
+    },
+    showAndHideSnackbar() {
+      this.snackbar = true;
+      setTimeout(() => {
+        this.snackbar = false;
+      }, 4000);
     }
   }
 });
--- pokerapp/src/views/Signup.vue
+++ pokerapp/src/views/Signup.vue
@@ -1,19 +1,8 @@
 <template>
   <div>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
 
-        <span>Online Poker</span>
-        <span class="font-weight-light"> - can you be the best player?!</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-       <v-btn flat to="/login">
-        Login
-      </v-btn>
-      <v-btn flat to="/signup">
-        Signup
-      </v-btn>
-    </v-toolbar>
+   <Navbar></Navbar>
+
     <v-img
       src="https://picjumbo.com/wp-content/uploads/playing-cards_free_stock_photos_picjumbo_DSC07167-2210x1473.jpg"
       max-height="700"
@@ -25,7 +14,7 @@
     >
       
       <SignupForm></SignupForm>
-      <v-btn color="primary" small depressed to="/signup">Already have account? Login here!</v-btn>
+      <v-btn color="primary" small depressed to="/login">Already have account? Login here!</v-btn>
     </v-layout>
     </v-img>
   </div>
@@ -34,11 +23,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import SignupForm from '../components/SignupForm.vue';
+import Navbar from '../components/Navbar.vue';
 
 export default Vue.extend({
   name: 'signup',
   components: {
-    SignupForm
+    SignupForm,
+    Navbar
   },
   data: () => ({
 
@@ -46,7 +37,6 @@ export default Vue.extend({
   methods: {
 
   },
-
 });
 </script>
 
