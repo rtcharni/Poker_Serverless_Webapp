10543d7465c0d9d3d71c9b89afdd3b1f66ae4ef4
elama09 <roman.tcharni@gmail.com>
2019-06-09 19:10:40 +0300

    Moved navbar to own component

---
+++ pokerapp/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-toolbar >
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Online Poker</span>
+        <span class="font-weight-light">- can you be the best player?!</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat to="/login">Login</v-btn>
+      <v-btn flat to="/signup">Signup</v-btn>
+    </v-toolbar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "navbar",
+  components: {},
+  data: () => ({}),
+  methods: {}
+});
+</script>
+
+<style scoped>
+</style>
+
