81358c192f4c2d3180c7dc0eb27566a7985cfb3e
elama09 <roman.tcharni@gmail.com>
2019-06-21 11:08:23 +0300

    Added deck and card interfaces

---
+++ pokerapp/src/models/interfaces.ts
@@ -0,0 +1,28 @@
+export interface Card {
+    suit: Suit;
+    value: Value;
+    name: string;
+}
+
+export enum Suit {
+    Spades = 1,
+    Clubs = 2,
+    Diamonds = 3,
+    Hearts = 4,
+}
+
+export enum Value {
+    Ace = 1 || 14,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+}
