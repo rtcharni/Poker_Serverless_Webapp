eb57730c662846687eaaa1f825f7c2d476bdca7f
elama09 <roman.tcharni@gmail.com>
2019-07-10 21:17:35 +0300

    Added sound and few functions

--- pokerapp/src/utils/utils.ts
+++ pokerapp/src/utils/utils.ts
@@ -1,5 +1,17 @@
-// tslint:disable-next-line: no-var-requires
 const cardDealSound = new Audio(require('../assets/sounds/Card-flip-sound-effect.mp3'));
 cardDealSound.volume = 0.5;
-export default cardDealSound;
+export { cardDealSound };
 
+const winSound = new Audio(require('../assets/sounds/Sound-of-coin.mp3'));
+winSound.volume = 0.5;
+export { winSound };
+
+export function soundsOff() {
+    cardDealSound.volume = 0;
+    winSound.volume = 0;
+}
+
+export function soundsOn() {
+    cardDealSound.volume = 0.5;
+    winSound.volume = 0.5;
+}
