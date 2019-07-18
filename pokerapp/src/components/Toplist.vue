a4d5eddae0b70d3350dfd4b9bd9cabf707c25ba6
elama09 <roman.tcharni@gmail.com>
2019-07-18 18:53:27 +0300

    Added toplist modal component

---
+++ pokerapp/src/components/Toplist.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="text-xs-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Poker Toplist
+        </v-card-title>
+
+        <v-card-text>
+          Lorem ipsum dolor sit at laba pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat
+            @click="dialog = false"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "toplist",
+  components: {},
+  props: {
+
+  },
+  data: () => ({
+      dialog: false
+  }),
+  methods: {
+  }
+});
+</script>
+
+<style scoped>
+</style>
+
